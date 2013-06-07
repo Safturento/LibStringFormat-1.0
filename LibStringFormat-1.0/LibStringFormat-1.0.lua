e7f61ce4a9cf459936d604cf8f36fcecb4ed504b
Jean-Michel Mailloux-Huberdeau <safturento@gmail.com>
2013-06-07 18:58:03 -0500

    Bug fixes and organization

--- LibStringFormat-1.0/LibStringFormat-1.0.lua
+++ LibStringFormat-1.0/LibStringFormat-1.0.lua
@@ -1,13 +1,13 @@
 --[=[
-	--[[
+
 	Name: LibStringFormat-3.0
 	Revision: $Revision: 1 $
 	Author: Safturento
 	Website: www.safturento.com
-	Description: Adds a variety of functions used for formatting strings and numbers easily
+	Description: Adds a variety of functions used to easily format strings and numbers 
 	Dependencies: LibStub
 	License: LGPL v2.1
-	]]
+
 
 	SanitizeNumber(stringOrNumber [,min [,max]]): removes any character that would render the number invalid.
 	  optionally, you can pass a min and max to give the number a size limitation
@@ -59,34 +59,35 @@ local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end -- No Upgrade needed.
 
 local min, max = math.min, math.max
-local tonumber, type, gsub = tonumber, type, gsub
+local tonumber, type = tonumber, type
+local gsub, strmatch, strfind = string.gsub, string.match, string.find
 local GOLD_COLOR = {gold='|cFFFFD700', silver='|cFFC7C7C7', copper='|cFFEDA55F'}
 
-function lib:SanitizeNumber(stringOrNumber, min, max)
+
+-------------------------------
+-- NUMBER FUNCTIONS -----------
+-------------------------------
+
+function lib:SanitizeNumber(stringOrNumber, minimum, maximum)
 	local num
-	if type(stringorint) == 'string' then
-		num = tonumber(gsub(stringOrNumber,'%D',''))
+	if type(stringOrNumber) == 'string' then
+		num = gsub(stringOrNumber, '%D','')
+		num = tonumber(num, 10)
+	else
+		num = stringOrNumber
 	end
 
-	if min then num = max(min, num) end
-	if max then num = min(max, num) end
+
+	if minimum then num = math.max(minimum, num) end
+	if maximum then num = math.min(maximum, num) end
 
 	return num
-end
-
-
-function lib:Trim(string)
-	local from = string:match"^%s*()"
-	return from > #string and "" or string:match(".*%S", from)
 end
 
 function lib:Round(val, decimals)
   local mult = 10^(decimals or 0)
   return math.floor(val * mult + 0.5) / mult
 end
-
-
-
 
 function lib:ToClock(sec)
 	local hour, minute = 3600, 60
@@ -99,64 +100,24 @@ end
 
 function lib:ToTime(sec)
 	local day, hour, minute = 86400, 3600, 60
-	if s >= day then
-		return format("%dd", ceil(s / day))
-	elseif s >= hour then
-		return format("%dh", ceil(s / hour))
-	elseif s >= minute then
-		return format("%dm", ceil(s / minute))
-	elseif s >= minute / 12 then
-		return floor(s)
+	if sec >= day then
+		return format("%dd", ceil(sec / day))
+	elseif sec >= hour then
+		return format("%dh", ceil(sec / hour))
+	elseif sec >= minute then
+		return format("%dm", ceil(sec / minute))
+	elseif sec >= minute / 12 then
+		return floor(sec)
 	end
-	return format("%.1f", s)
+	return format("%.1f", sec)
 end
-
-
-function lib:ToHex(r, g, b)
-	r = r <= 1 and r >= 0 and r or r/255
-	g = g <= 1 and g >= 0 and g or g/255
-	b = b <= 1 and b >= 0 and b or b/255
-	return string.format("%02x%02x%02x", r*255, g*255, b*255)
-end
-
-
-function lib:ToRGB(hexString, divide)
-	local rHex, gHex, bHex
-	if strlen(hexString) == 6 then
-		rHex, gHex, bHex = strmatch(hexString, '([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})')
-	elseif strlen(hexString) == 3 then
-		rHex, gHex, bHex = strmatch(hexString, '([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])')
-		if rHex and gHex and bHex then
-			rHex = rHex .. rHex
-			gHex = gHex .. gHex
-			bHex = bHex .. bHex
-		end
-	end
-	
-	assert(rHex and gHex and bHex, MAJOR..'Invalid hexString parameter passed for ToRGB function.')
-
-	local r, g, b = tonumber(rHex, 16), tonumber(gHex, 16), tonumber(bHex, 16)
-	if divide then r = r/255; g=g/255; b=b/255 end
-	return r, g, b
-end
-
-
-function lib:ColorString(string, ...)
-	-- if only one parameter is passed to the varargs, assume a hex
-	--	value was passed. Otherwise, assume RGB values were passed.
-	local hex = #{...} == 1 and ... or self:ToHex(...)
-
-	return format('|cff%s%s|r', hex, string)
-end
-
 
 function lib:ShortFormat(value, decimals)
 	local suffix = 'KMBTQ' --I doubt you'd need more than quadrillions..
-	local factor = floor(strlen(value)-1 / 3)
+	local factor = floor((strlen(value)-1) / 3)
 	local converted = value/math.pow(1000,factor)
 	return format('%.0'..(decimals or 0)..'f%s', converted, strsub(suffix, factor, factor))
 end
-
 
 function lib:FileSizeFormat(bytes, decimals)
     local suffix = 'kMGTPEZY'
@@ -164,18 +125,6 @@ function lib:FileSizeFormat(bytes, decimals)
     local converted = bytes/math.pow(1024,factor)
     return format('%.0'..(decimals or 0)..'f%sB', converted, strsub(suffix, factor, factor))
 end
-
-
-function lib:CommaFormat(value)
-	while true do  
-		value, k = string.gsub(value, "^(-?%d+)(%d%d%d)", '%1,%2')
-		if (k==0) then break end
-	end
-	return value
-end
-
-
-
 function lib:GoldFormat(money, round)
 	local negative = money<0 and true or false --if money is negative, add a negative sign when returning the string
 	money = (abs(money)) --remove the negative sign from the actual value
@@ -202,4 +151,58 @@ function lib:GoldFormat(money, round)
 
 		return gold..silver..copper
 	end
+end
+
+-------------------------------
+-- STRING FUNCTIONS -----------
+-------------------------------
+
+function lib:Trim(string)
+	return string:find('^%s*$') and '' or string:match('^%s*(.*%S)')
+end
+function lib:CommaFormat(value)
+	while true do  
+		value, k = string.gsub(value, "^(-?%d+)(%d%d%d)", '%1,%2')
+		if (k==0) then break end
+	end
+	return value
+end
+
+-------------------------------
+-- COLOUR FUNCTIONS -----------
+-------------------------------
+
+function lib:ToHex(r, g, b)
+	r = r <= 1 and r >= 0 and r or r/255
+	g = g <= 1 and g >= 0 and g or g/255
+	b = b <= 1 and b >= 0 and b or b/255
+	return string.format("%02x%02x%02x", r*255, g*255, b*255)
+end
+
+function lib:ToRGB(hexString, divide)
+	local rHex, gHex, bHex
+	if strlen(hexString) == 6 then
+		rHex, gHex, bHex = strmatch(hexString, '([a-fA-F0-9][a-fA-F0-9])([a-fA-F0-9][a-fA-F0-9])([a-fA-F0-9][a-fA-F0-9])')
+	elseif strlen(hexString) == 3 then
+		rHex, gHex, bHex = strmatch(hexString, '([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])')
+		if rHex and gHex and bHex then
+			rHex = rHex .. rHex
+			gHex = gHex .. gHex
+			bHex = bHex .. bHex
+		end
+	end
+	
+	assert(rHex and gHex and bHex, MAJOR..'Invalid hexString parameter passed for ToRGB function.')
+
+	local r, g, b = tonumber(rHex, 16), tonumber(gHex, 16), tonumber(bHex, 16)
+	if divide then r = r/255; g=g/255; b=b/255 end
+	return r, g, b
+end
+
+function lib:ColorString(string, ...)
+	-- if only one parameter is passed to the varargs, assume a hex
+	--	value was passed. Otherwise, assume RGB values were passed.
+	local hex = #{...} == 1 and ... or self:ToHex(...)
+
+	return format('|cff%s%s|r', hex, string)
 end
